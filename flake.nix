eaa94183676b08d40f1a2556e49d12ac5343d8b7
Frederik Richter <frederik.richter@mailbox.org>
2024-10-13 19:39:04 +0200

    added dependencies

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nix remote builder script that really builds on remote and copies result to local store";
+  description = "Nix remote builder script that builds on remote and copies result to local store";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Use the latest nixpkgs
@@ -9,11 +9,27 @@
     system = "x86_64-linux"; # Change this if you're on a different architecture
     pkgs = import nixpkgs { inherit system; };
   in {
-    packages.${system}.nix-remote-build = pkgs.writeShellScriptBin "nix-remote-build" ''
-    #!${pkgs.bash}/bin/bash
-      ${./nix-remote-build} "$@"
-    '';
-    
+    packages.${system}.nix-remote-build = pkgs.stdenv.mkDerivation {
+      name = "nix-remote-build";
+
+      # Specify the script as the source
+      src = ./.;
+
+      buildInputs = [
+        pkgs.openssh 
+        pkgs.git        
+        pkgs.openssl
+        pkgs.bash
+      ];
+
+      # Ensure that the script is executable
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ${./nix-remote-build} $out/bin/nix-remote-build
+        chmod +x $out/bin/nix-remote-build
+      '';
+    };
+
     defaultPackage.${system} = self.packages.${system}.nix-remote-build;
   };
 }
